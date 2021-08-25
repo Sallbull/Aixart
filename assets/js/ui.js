25b4b44fcd241124ef63b3301ebb9b543a2bb9cb
martin <martin@irm.kr>
2021-08-26 05:28:27 +0900

    모바일 작업중

---
+++ assets/js/ui.js
@@ -0,0 +1,45 @@
+var $winW, $winH, $isMobile;
+$(function () {
+
+    // ===== Scroll to Top ==== 
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 200) {
+            $('.header').addClass('sticky');
+        } else {
+            $('.header').removeClass('sticky');
+        }
+    });
+
+
+    
+    if ($('.top.swiper-container').length) {
+        var swiper = new Swiper('.top.swiper-container', {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            loop: true,
+            // autoplay: {
+            //     delay: 5000,
+            //     disableOnInteraction: false,
+            // },
+            // navigation: {
+            //     nextEl: '.__top .swiper-button-next',
+            //     prevEl: '.__top .swiper-button-prev',
+            // },
+            pagination: {
+                el: '.top .swiper-pagination',
+                clickable: true,
+            },
+        });
+    }
+    
+    $('.qna_list .q').on('click',function(){
+        $(this).toggleClass('on');
+        $(this).next('.a').toggleClass('on');
+
+    });
+});
+
+
+    
+
+
