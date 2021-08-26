81ecbabb70dbd94895006e603507eb92611d8849
martin <martin@irm.kr>
2021-08-26 16:17:25 +0900

    menu 완성

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,6 +1,11 @@
 var $winW, $winH, $isMobile;
 $(function () {
 
+    chkBrowserSize();
+
+    $(window).resize(function () {
+        chkBrowserSize();
+    });
     // ===== Scroll to Top ==== 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 200) {
@@ -8,6 +13,25 @@ $(function () {
         } else {
             $('.header').removeClass('sticky');
         }
+    });
+    $('nav li').off('click').on('click', function () {
+        let $headerHeight = $('.header').height();
+        let _target = $(this).attr('rel');
+        let moveTop = $('#' + _target).offset().top - $headerHeight;
+        
+        $(this).addClass('on').siblings().removeClass('on');
+
+        if ($isMobile) {
+            
+            moveTop = $('#' + _target).offset().top - $headerHeight;
+            // $('body,html').scrollTop(moveTop);
+            $('.nav').removeClass('on');
+            $('#wrap').removeClass('on');
+        }
+        $('body,html').animate({
+            scrollTop: moveTop  //탑 설정 클수록 덜올라간다
+        }, 1000, 'easeInOutExpo');  // 탑 이동 스크롤 속도를 조절할 수 있다.
+        return false;
     });
 
 
@@ -40,7 +64,7 @@ $(function () {
 
     $('#btnMobile').on('click',function(){
         $('.nav').addClass('on');
-        $('#wrap').addClass('on');
+        // $('#wrap').addClass('on');
     });
 
     $('#mClosed').on('click',function(){
@@ -52,6 +76,15 @@ $(function () {
 });
 
 
-    
+function chkBrowserSize() {
+    $winW = $(window).width();
+    $winH = $(window).height();
+
+    if ($winW < 1200) {
+        $isMobile = true;
+    } else {
+        $isMobile = false;
+    }
+}    
 
 
